958f14e621c6b177e0adc50e9f1b2902326acbea
Jonas Fluhrer <jonas.fluhrer@novatec-gmbh.de>
2022-04-11 11:50:18 +0200

    add standalone mode to vite-app

--- vite-app/src/main.js
+++ vite-app/src/main.js
@@ -3,28 +3,34 @@ import App from "./App.vue";
 import singleSpaVue from "single-spa-vue";
 import router from "./router";
 
-// createApp(App).mount('#app')
+let vueLifecycles;
 
-const vueLifecycles = singleSpaVue({
-  createApp,
-  appOptions: {
-    render() {
-      return h(App, {
-        props: {
-          // single-spa props are available on the "this" object. Forward them to your component as needed.
-          // https://single-spa.js.org/docs/building-applications#lifecyle-props
-          name: this.name,
-          mountParcel: this.mountParcel,
-          singleSpa: this.singleSpa,
-        },
-      });
+if (import.meta.env.MODE === "standalone") {
+  const app = createApp(App);
+  app.use(router);
+  app.mount("#app");
+} else {
+  vueLifecycles = singleSpaVue({
+    createApp,
+    appOptions: {
+      render() {
+        return h(App, {
+          props: {
+            // single-spa props are available on the "this" object. Forward them to your component as needed.
+            // https://single-spa.js.org/docs/building-applications#lifecyle-props
+            name: this.name,
+            mountParcel: this.mountParcel,
+            singleSpa: this.singleSpa,
+          },
+        });
+      },
     },
-  },
-  handleInstance: (app) => {
-    app.use(router);
-  },
-});
+    handleInstance: (app) => {
+      app.use(router);
+    },
+  });
+}
 
-export const bootstrap = vueLifecycles.bootstrap;
-export const mount = vueLifecycles.mount;
-export const unmount = vueLifecycles.unmount;
+export const bootstrap = vueLifecycles?.bootstrap;
+export const mount = vueLifecycles?.mount;
+export const unmount = vueLifecycles?.unmount;
